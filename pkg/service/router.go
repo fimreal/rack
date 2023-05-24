f7a5eddbab6b2fd71629513ed6b07fe2abf8cc06
fimreal <6913651@qq.com>
2023-05-24 11:25:51 +0800

    del ip2location

--- pkg/service/router.go
+++ pkg/service/router.go
@@ -3,7 +3,6 @@ package service
 import (
 	"github.com/fimreal/rack/pkg/components/swagger"
 	"github.com/fimreal/rack/pkg/service/fileserver"
-	"github.com/fimreal/rack/pkg/service/ip2location"
 	"github.com/gin-gonic/gin"
 	"github.com/spf13/viper"
 )
@@ -17,11 +16,6 @@ func AddRoutes(r *gin.Engine) {
 	}
 	if viper.GetBool("swagger") || pass {
 		swagger.AddRoutes(r)
-	}
-	if viper.GetBool("ip2location") || pass {
-		rg := r.Group("/ip2location")
-		rg.GET("/:ip", ip2location.IpQuery)
-		rg.POST("/:ip", ip2location.IpQuery)
 	}
 
 }
